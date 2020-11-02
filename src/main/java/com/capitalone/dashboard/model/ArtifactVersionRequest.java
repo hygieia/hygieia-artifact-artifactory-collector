d6a909bbcf24d558a09ec41a9a3ced194cba39e5
nireeshT <nireesh.thiruveedula@capitalone.com>
2020-11-01 21:16:25 -0500

    Artifactory api for version

---
+++ src/main/java/com/capitalone/dashboard/model/ArtifactVersionRequest.java
@@ -0,0 +1,71 @@
+package com.capitalone.dashboard.model;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+
+public class ArtifactVersionRequest {
+
+    private String artifactVersion;
+    private String artifactName;
+    private String repoName;
+    private String path;
+    private String instanceUrl;
+    private long startTime;
+    private long endTime;
+
+    public String getArtifactVersion() {
+        return artifactVersion;
+    }
+
+    public void setArtifactVersion(String artifactVersion) {
+        this.artifactVersion = artifactVersion;
+    }
+
+    public String getArtifactName() {
+        return artifactName;
+    }
+
+    public void setArtifactName(String artifactName) {
+        this.artifactName = artifactName;
+    }
+
+    public String getRepoName() {
+        return repoName;
+    }
+
+    public void setRepoName(String repoName) {
+        this.repoName = repoName;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getInstanceUrl() {
+        return instanceUrl;
+    }
+
+    public void setInstanceUrl(String instanceUrl) {
+        this.instanceUrl = instanceUrl;
+    }
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(long startTime) {
+        this.startTime = startTime;
+    }
+
+    public long getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(long endTime) {
+        this.endTime = endTime;
+    }
+}
